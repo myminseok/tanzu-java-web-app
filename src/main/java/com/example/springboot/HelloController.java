d8d612d1ee198d3bb689ca59f8a6b498249f9016
myminseok <myminseok@gmail.com>
2022-11-09 13:40:36 +0900

    cors

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,8 +2,10 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class HelloController {
 
 	@RequestMapping("/")
