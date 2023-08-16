2309720ddf7d1e29ae58b9e422ec20a2059f9f24
Minseok Kim <myminseok@users.noreply.github.com>
2023-08-16 13:11:40 +0900

    Update HelloController.java

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
 
 	@RequestMapping("/")
 	public String index() {
-		return "Greetings from Spring Boot + Tanzus2!";
+		return "Greetings from Spring Boot + Tanzus3!";
 	}
 
 }
